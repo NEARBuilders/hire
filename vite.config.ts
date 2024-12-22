9df9b7a67f296149726d7376c462698f5ea49988
Elliot Braem <elliot@ejlbraem.com>
2024-12-22 12:43:36 -0600

    rewrites

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import vercel from "vite-plugin-vercel";
-import ssr from "vike/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ssr({
-      prerender: true,
-    }),
-    vercel({
-      rewrites: [
-        {
-          source: "/((?!api/.*).*)",
-          destination: "/index.html"
-        }
-      ]
-    })
-  ],
+  plugins: [react(), vercel({
+    "rewrites": [
+      {
+        "source": "/(.*)",
+        "destination": "/index.html"
+      }
+    ]
+  })],
   optimizeDeps: {
     exclude: ["lucide-react"],
   },

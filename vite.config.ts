546b1ecac77d47dc1f79e2798e99cf3791270965
Elliot Braem <elliot@ejlbraem.com>
2024-12-22 12:34:52 -0600

    ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import vercel from "vite-plugin-vercel";
+import ssr from "vike/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    ssr({
+      prerender: true,
+    }),
     vercel({
       rewrites: [
         {
